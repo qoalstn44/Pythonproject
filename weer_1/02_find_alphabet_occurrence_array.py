5839e926a171bd8d3017f18ffdaf468a125ee3d6
qoalstn44 <qoalstn44@naver.com>
2022-11-09 22:40:01 +0900

    first commit

---
+++ weer_1/02_find_alphabet_occurrence_array.py
@@ -0,0 +1,11 @@
+def find_alphabet_occurrence_array(string):
+    alphabet_occurrence_array = [0] * 26
+
+    for char in string:
+        if not char.isalpha():
+            continue
+        arr_index = ord(char) - ord("a")
+        alphabet_occurrence_array[arr_index] += 1
+
+
+print(find_alphabet_occurrence_array("hello my name is sparta"))
